93ec18c15c90754e6a2b66f18d4e7ee9c05aed72
Victor Ruiz <ruizdvictor@gmail.com>
2017-12-24 23:09:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function sayHiToGrandma(string) {
     return "I can't hear you!";
   } 
   else if (string === string.toUpperCase()){
-    return "YES INDEED";
+    return "YES INDEED!";
   }
   else if (string === "I love you, Grandma.") {
     return "I love you, too."
